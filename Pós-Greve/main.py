039ca1171b71e423632968871480e0a66b49f2cd
Ani <148493441+AnaBeatriz190@users.noreply.github.com>
2024-08-10 14:41:43 +0000

    salvamento 1.0

---
+++ Pós-Greve/main.py
@@ -0,0 +1,12 @@
+def saudacao(nome):
+    """
+    Escreva uma função chamada saudacao que aceita um nome 
+    como argumento e imprime "Olá, [nome]!".
+    """
+    return (f"Olá, {nome}!")
+
+def dobro():
+    """
+    Crie uma função dobro que aceita um número 
+    como argumento e retorna o dobro desse número.
+    """
